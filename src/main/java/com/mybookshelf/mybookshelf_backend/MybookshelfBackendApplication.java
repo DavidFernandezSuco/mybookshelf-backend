a28d17815bd162b80eb7a0f294dce89ae515b81c
DavidFernandezSuco <dfsuco@gmail.com>
2025-08-20 15:01:50 +0200

    feat: initial Spring Boot project setup with all dependencies

---
+++ src/main/java/com/mybookshelf/mybookshelf_backend/MybookshelfBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mybookshelf.mybookshelf_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybookshelfBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybookshelfBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mybookshelf/mybookshelf_backend/MybookshelfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybookshelf.mybookshelf_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybookshelfBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
